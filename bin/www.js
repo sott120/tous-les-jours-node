7d9e8a146b641c1463cfb57195921c85bca07404
sott120 <psy_sott120@naver.com>
2022-06-23 16:38:05 +0900

    bin port수정

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,5 @@
 var app = require('../app.js'); //app,js에 있는 내용 실행
-var port = 3000; //3000번 포트 실행
+var port = process.env.PORT || 3000; //3000번 포트 실행
 
 app.listen(port, () => { //서버 실행
     console.log(`express 실행 ${port}`) //서버가 실행됐을 때 console창에 나타남
