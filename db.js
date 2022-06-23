084c0f963bbc75d984b9788b07788b63cefbf8bb
sott120 <psy_sott120@naver.com>
2022-06-23 16:24:29 +0900

    db 정보수정

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'us-cdbr-east-05.cleardb.net',
     user: 'b96b593c28ad68',
-    password: '1234',
-    port: 'fe2977c9',
+    password: 'fe2977c9',
+    port: '3306',
     database: 'heroku_aa6031eb311b2b4',
     dateStrings: 'date'
 }); //보안상 다른 파일로 빼야됨
